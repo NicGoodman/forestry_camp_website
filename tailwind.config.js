0e748989aaad774f5a2f4875d734ef3184c37fd1
Nic <nic@ninebarcreative.com>
2019-07-15 13:48:39 -0400

    Installed and Customized Tailwindcss 1.0.4

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        FoxCat: ['Fox Cat', 'sans-serif'],
+        GuestCheck: ['Guest Check', 'sans-serif'],
+      },
+      colors: {
+        fc: {
+          black: '#231f20',
+          dark_green: '#183a1c',
+          olive_green: '#313a18',
+          green: '#265529',
+          tan: '#9b9472',
+          red: '#ae2724',
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
